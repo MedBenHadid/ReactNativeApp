0a86466aefc4d8d38411404644dabec39e68111e
MedBenHadid <benhdid21@gmail.com>
2019-01-21 12:40:13 +0100

    2nd Commit

--- App.js
+++ App.js
@@ -5,7 +5,6 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up  to start working on your app!</Text>
       </View>
     );
   }
